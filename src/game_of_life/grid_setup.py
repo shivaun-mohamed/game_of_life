37ee40ff87ae1a4ce97c31eb102646e5d8264a05
shivaun.mohamed <shivaun.mohamed@ucalgary.ca>
2026-07-13 16:24:02 -0400

    In progress: grid_setup logic, still debugging cell append bug

--- src/game_of_life/grid_setup.py
+++ src/game_of_life/grid_setup.py
@@ -9,3 +9,33 @@
 # send info to display_grid
 
 # NOTE: update logic
+
+# LOGIC:
+# first it generate the dimensions of the grid randomly (num_rows, num_cols)
+# create empty 2D array
+# iterate through rows and columns and build cells
+# for each cell, give 25% mortality rate - alive(1) or dead(0)
+# Once row is built, append to empty 2D array
+# Once all rows are built, return grid
+
+from random import randint, random
+
+# generate dimensions
+num_rows = randint(20,100)
+num_cols = randint(20,100)
+
+# create empty 2D array
+grid = []
+
+# iterate through rows and columns and build cells
+for i in range(num_rows):
+    current_row = []
+    for j in range(num_cols):
+        current_row.append(random())
+
+        if current_row[0] <= 0.25:
+            grid.append(1)
+        else:
+            grid.append(0)
+
+print(grid)            
