8d74aef6e91d4a040bdbf79e870c3a851588dd70
shivaun.mohamed <shivaun.mohamed@ucalgary.ca>
2026-07-13 22:58:03 -0400

    wrap grid_setup.py in create_grid() function, removed print(grid) and replaced with return grid for main.py to import grid when needed

--- src/game_of_life/grid_setup.py
+++ src/game_of_life/grid_setup.py
@@ -1,16 +1,6 @@
 # grid_setup.py deals with the game states of each generation
 
 # LOGIC:
-# first it determines the dimensions of the grid (number of rows, number of columns)
-    # -> on initial state it does this
-# iterates through rows and columns in which i and j coordinates are placed in a 2D array list
-    # -> gets info from compute_next generation after initial state
-# determines where the living cell is (need to determine if cell is living, maybe if cell contains something, then it is living)
-# send info to display_grid
-
-# NOTE: update logic
-
-# LOGIC:
 # first it generate the dimensions of the grid randomly (num_rows, num_cols)
 # create empty 2D array
 # iterate through rows and columns and build cells
@@ -20,25 +10,34 @@
 
 from random import randint, random
 
-# generate dimensions
-num_rows = randint(20,100)
-num_cols = randint(20,100)
+def create_grid():
+    
+    # generate dimensions
+    num_rows = randint(20,100)
+    num_cols = randint(20,100)
 
-# create empty 2D array
-grid = []
+    # create empty 2D array to hold grid
+    grid = []
 
-# iterate through rows and columns and build cells
-for i in range(num_rows):
-    current_row = []
-    for j in range(num_cols):
+    # iterate through rows and columns and build cells
+    for i in range(num_rows):
 
-        mortality = random()
+        # build array to hold current row
+        current_row = []
+        for j in range(num_cols):
+            
+            # random float between 0 and 1
+            mortality = random()
 
-        if mortality <= 0.25:
-            current_row.append(1)
-        else:
-            current_row.append(0)
+            # add mortality value to current row for 25% mortality rate
+            if mortality <= 0.25:
+                current_row.append(1)
+            else:
+                current_row.append(0)
 
-    grid.append(current_row)
+        # add current row to grid
+        grid.append(current_row)
 
-print(grid)            
+    print(grid)
+
+create_grid()
